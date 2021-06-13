2a867305a5ae3c25dd4a4e110fc0fdde53c8fd0d
shanthshivam <shanthshivam@gmail.com>
2021-06-13 08:45:54 +0100

    first commit

---
+++ src/test/java/com/ashwin/JsonParser1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ashwin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JsonParser1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
